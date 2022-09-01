aa285b2813a2281923bde5c990cc466ee7e46f47
MohammedHaq <mohammed.zainh@gmail.com>
2022-09-01 16:00:27 -0400

    added input elements and button to webpage

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className = "InventoryInputs">
+        <label>Product Name:</label>
+        <input type="text"/>
+        <label>Product Code:</label>
+        <input type="text"/>
+        <label>Brand:</label>
+        <input type="text"/>
+        <label>Price:</label>
+        <input type="number"/>
+        <label>Quantity:</label>
+        <input type="number"/>
+        <label>Date Added:</label>
+        <input type="text"/>
+        <button>Add Item</button>
+      </div>
     </div>
   );
 }
