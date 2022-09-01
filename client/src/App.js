ebfd0652e4be08bd8582da8cf6e6ce46723de758
MohammedHaq <mohammed.zainh@gmail.com>
2022-09-01 17:35:28 -0400

    added react states for inputs

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ProductName, setName] = useState("");
+  const [ProductCode, setCode] = useState("");
+  const [Brand, setBrand] = useState("");
+  const [Price, setPrice] = useState(0);
+  const [Quantity, setQuantity] = useState(0);
+  const [Date, setDate] = useState("");
+
   return (
     <div className="App">
       <div className = "InventoryInputs">
         <label>Product Name:</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {setName(event.target.value)}}/>
         <label>Product Code:</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {setCode(event.target.value)}}/>
         <label>Brand:</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {setBrand(event.target.value)}}/>
         <label>Price:</label>
-        <input type="number"/>
+        <input type="number" onChange={(event) => {setPrice(event.target.value)}}/>
         <label>Quantity:</label>
-        <input type="number"/>
+        <input type="number" onChange={(event) => {setQuantity(event.target.value)}}/>
         <label>Date Added:</label>
-        <input type="text"/>
+        <input type="text" onChange={(event) => {setDate(event.target.value)}}/>
         <button>Add Item</button>
       </div>
     </div>
